026f3dc9748ef1e650119c0a1c75db1e5c91abda
Dan <dannanasi@gmail.com>
2024-04-08 23:02:21 -0700

    yolo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react"
 
-import Nav from "../components/nav";
+import Nav from "../components/nav"
 
 import Cloud from "../images/cloud.png"
 import Forest from "../images/forest.png"
@@ -9,11 +9,56 @@ import Salmon from "../images/salmon.png"
 import Sky from "../images/sky.png"
 
 const Index = () => {
-	return(
-		<>
+
+
+	const pattern = [
+		[4, 5, 2, 0, 0],
+		[4, 5, 2, 2, 0],
+		[0, 3, 2, 3, 0],
+		[0, 2, 1, 2, 1],
+		[0, 1, 3, 4, 3],
+		[0, 0, 2, 3, 4],
+	]
+
+	useEffect(() => {
+
+		let timeout
+		let index = 0
+
+		const nextSlide = () => {
+
+			clearTimeout(timeout)
+	
+			index += 1
+			index %= pattern.length
+
+			document.getElementById("slide-container").style.gridTemplateColumns = pattern[index].map((c) => `${c}fr`).join(" ")
+			const slides = document.querySelectorAll('.slide-element')
+	
+			slides.forEach((slide, slideIndex) => {
+				if (pattern[index][slideIndex] === 0) {
+					slide.classList.add("hide")
+				} else {
+					slide.classList.remove("hide")
+				}
+			})
+
+			timeout = setTimeout(nextSlide, 2000)
+
+		}
+
+		timeout = setTimeout(nextSlide, 2000)
+
+		document.getElementById("slide-container").addEventListener("click", nextSlide)
+
+	})
+
+	
+	return (
+		<div className="absolute w-full h-full flex flex-col">
 			<Nav></Nav>
-			<main className="home-main">
-				<section className="slide-container">
+			<main className="home-main grow flex items-end">
+				<section className="slide-container" id="slide-container">
 					<div className="slide-element">
 						<figure className="slide-figure">
 							<figcaption className="slide-figure-caption">testing</figcaption>
@@ -46,7 +91,7 @@ const Index = () => {
 					</div>
 				</section>
 			</main>
-		</>
+		</div>
 	)
 }
 
