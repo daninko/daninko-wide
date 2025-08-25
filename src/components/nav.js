a569cb2a2a47a8b9fbd396ed0a8c3e857987a2b4
Dan Nanasi <dannanasi@gmail.com>
2025-08-25 00:29:23 -0400

    whatever

--- src/components/nav.js
+++ src/components/nav.js
@@ -6,10 +6,10 @@ const Nav = ({ page = "/", isHome = true, data }) => {
 	return (
 		<nav className="flex h-[100dvh] max-w-[800px] flex-[0 0 auto]">
 			<motion.div layout className="w-[400px] bg-[#f9f9f9]" whileHover={{width:"420px"}} style={{cursor: "pointer"}}>
-				<div className="my-[200px] ml-[50px] text-[#111111]">Dan Nanasi</div>
+				<div className="my-[15dvh] ml-[50px] text-[#111111]">Dan Nanasi</div>
 			</motion.div>
 			<motion.div layout className="w-[400px] bg-[#111111]">
-				<div className="my-[200px] ml-[50px] text-[#f9f9f9]">
+				<div className="my-[15dvh] ml-[50px] text-[#f9f9f9]">
 					<span className="block">Product Design </span>
 					<span className="block opacity-[0.5]">at Microsoft</span>
 				</div>
